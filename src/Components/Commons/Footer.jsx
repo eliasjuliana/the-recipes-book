fa0e3db48b15ec1f465f21457197e334a2c77c0a
Juliana Elias <eliasjuliana@gmail.com>
2023-10-17 11:47:54 -0300

    navbar y footer

--- src/Components/Commons/Footer.jsx
+++ src/Components/Commons/Footer.jsx
@@ -1,6 +1,11 @@
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className="container-fluid bg-dark">
+        <section className="py-5 text-light">
+            <h4>Inutilisimas</h4>
+            <p>&copy; Todos los derechos reservados</p>
+        </section>
+    </footer>
   )
 }
 
--- src/Components/Commons/Navbar.jsx
+++ src/Components/Commons/Navbar.jsx
@@ -1,9 +1,42 @@
-
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header>Navbar</header>
-  )
-}
+    <header>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Inutilisimas
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarInutilisimas"
+            aria-controls="navbarInutilisimas"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarInutilisimas">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className={`nav-link ${({isActive})=>{isActive? 'active' : ''}}`} aria-current="page" to="/">
+                  Inicio
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className={`nav-link`} to="/login">
+                  Login
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
